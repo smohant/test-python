e4d3bf676dbddc92e5635da88ba00d4fadf3bee6
Sanjay Mohanty <sanjaymohanty2009@gmail.com>
2019-04-06 22:00:15 -0500

    First version

---
+++ sanjayutil/sanjayutil.py
@@ -0,0 +1,9 @@
+def greet(my_name):
+    """
+       Print a greeting message.
+       Args:
+          my_name (str): person's name
+       Returns:
+          None
+    """
+    print(f"Hello from Sanjay's world - {my_name}.")
